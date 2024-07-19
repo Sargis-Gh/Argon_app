7b1eba86045bf8b111dee8bf1dd92765d1a708f7
Sargis-Gh <sargis.ghalechyan1@gmail.com>
2024-07-19 17:44:38 +0400

    phase_1 first commit

---
+++ App.js
@@ -0,0 +1,28 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  useColorScheme,
+} from 'react-native';
+
+import {
+  Colors,
+
+} from 'react-native/Libraries/NewAppScreen';
+
+
+function App() {
+
+  return (
+    <SafeAreaView>
+    </SafeAreaView>
+  );
+}
+
+export default App;
